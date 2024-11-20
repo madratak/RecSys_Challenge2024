72ae78013a58962b7a2be145935fc5cefed6e725
madratak <madratak@gmail.com>
2024-11-20 13:48:46 +0100

    Put normalization in the _compute_item_score function

--- Recommenders/Hybrid/LinearCombinationRecommender.py
+++ Recommenders/Hybrid/LinearCombinationRecommender.py
@@ -6,6 +6,7 @@
 
 import numpy as np
 import time
+from numpy import linalg as LA
 
 from Recommenders.BaseRecommender import BaseRecommender
 from Recommenders.BaseSimilarityMatrixRecommender import BaseItemSimilarityMatrixRecommender
@@ -22,13 +23,13 @@ class LinearCombinationRecommender(BaseItemSimilarityMatrixRecommender):
         super(LinearCombinationRecommender, self).__init__(URM_train)
 
         self.recommenders = recommenders
-        self.weights = None
         
         
-    def fit(self, weights: list[float]):
+    def fit(self, weights: list[float], norm):
         
         start_time = time.time()
         self.weights = np.array(weights)
+        self.norm = norm
         total_time = time.time() - start_time
         self._print(f"Fit completed in {total_time:.2f} seconds.")
 
@@ -38,6 +39,9 @@ class LinearCombinationRecommender(BaseItemSimilarityMatrixRecommender):
         scores = np.zeros((len(user_id_array), self.URM_train.shape[1]))
         
         for recommender, weight in zip(self.recommenders, self.weights):
-            scores += weight * recommender._compute_item_score(user_id_array, items_to_compute)
+            
+            item_scores = recommender._compute_item_score(user_id_array, items_to_compute)
+            norm_item_scores = LA.norm(item_scores, self.norm, axis=1, keepdims=True)            
+            scores += weight * (item_scores / norm_item_scores)
         
         return scores
