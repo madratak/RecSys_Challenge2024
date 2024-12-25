4b2ce0e7e240a5f5e298cf11ccbb8b56a241ad3e
madratak <madratak@gmail.com>
2024-12-25 16:17:43 +0100

    Upload XGBoost functions and class

---
+++ Recommenders/XGBoost/XGboostRankerRecommender.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Mauro Orazio Drago
+"""
+
+import numpy as np
+import scipy.sparse as sps
+from Recommenders.BaseRecommender import BaseRecommender
+from xgboost import XGBRanker
+
+class XGBoostRankerRecommender(BaseRecommender):
+    """XGBoost Ranker Recommender"""
+
+    RECOMMENDER_NAME = "XGBoostRankerRecommender"
+
+    def __init__(self, training_dataframe, verbose=True):
+        super(XGBoostRankerRecommender, self).__init__(verbose=verbose)
+        self.training_dataframe = training_dataframe
+        self.recommendations_dataframe = None
+        self.model = None
+
+    def set_recommendations_dataframe(self, recommendations_dataframe):
+        """
+        Update the training dataframe.
+
+        Parameters:
+        - new_training_dataframe: New dataframe to be used for training.
+        """
+        self.recommendations_dataframe = recommendations_dataframe
+
+    def fit(self, X_train, y_train, groups, n_estimators=50, learning_rate=1e-1, reg_alpha=1e-1, 
+            reg_lambda=1e-1, max_depth=5, max_leaves=0, grow_policy="depthwise", 
+            objective="pairwise", booster="gbtree", random_seed=None):
+        """
+        Train the XGBoost Ranker model.
+
+        Parameters:
+        - X_train: DataFrame of features for training.
+        - y_train: Labels indicating relevance.
+        - groups: Array of group sizes by user.
+        - n_estimators: Number of boosting rounds.
+        - learning_rate: Boosting learning rate.
+        - reg_alpha: L1 regularization term.
+        - reg_lambda: L2 regularization term.
+        - max_depth: Maximum tree depth.
+        - max_leaves: Maximum number of leaves per tree.
+        - grow_policy: Growth policy for trees.
+        - objective: Ranking objective function.
+        - booster: Type of booster to use.
+        - random_seed: Random seed for reproducibility.
+        """
+
+        # Initialize and train the XGBRanker
+        self.model = XGBRanker(
+            objective='rank:{}'.format(objective),
+            n_estimators=int(n_estimators),
+            random_state=random_seed,
+            learning_rate=learning_rate,
+            reg_alpha=reg_alpha,
+            reg_lambda=reg_lambda,
+            max_depth=int(max_depth),
+            max_leaves=int(max_leaves),
+            grow_policy=grow_policy,
+            verbosity=0,  # Adjust verbosity if needed
+            booster=booster,
+            enable_categorical=True,
+            tree_method="hist"  # Supported tree methods are `gpu_hist`, `approx`, and `hist`.
+        )
+
+        self.model.fit(
+            X_train,
+            y_train,
+            group=groups,
+            verbose=False
+        )
+
+    def _compute_item_score(self, user_id_array, items_to_compute=None):
+        """
+        Compute item scores for given users.
+
+        Parameters:
+        - user_id_array: Array containing the user indices.
+        - items_to_compute: Array containing items to compute scores for. If None, compute for all items.
+
+        Returns:
+        - Array of scores with shape (len(user_id_array), n_items).
+        """
+        scores = []
+        for user_id in user_id_array:
+            X_user = self.recommendations_dataframe[self.recommendations_dataframe['UserID'] == user_id]
+
+            # Filter by items_to_compute if provided
+            if items_to_compute is not None:
+                user_data = user_data[user_data['ItemID'].isin(items_to_compute)]
+
+            # Predict scores
+            user_scores = self.model.predict(X_user)
+            scores.append(user_scores)
+
+        return np.array(scores)
---
+++ Utils/xgboost_functions.py
@@ -0,0 +1,170 @@
+"""
+@author: Mauro Orazio Drago
+"""
+import numpy as np
+import pandas as pd
+import scipy.sparse as sps
+from tqdm import tqdm
+
+import json
+import os
+import time
+
+def fit_recommenders(metric, URM_train, ICM_all, recommenders, GH_PATH):
+    """
+    Fit recommenders with the best parameters for a specified evaluation metric.
+
+    Parameters:
+    - metric: Metric used for parameter optimization (e.g., "MAP" or "Recall").
+    - URM_train: User-Item interaction matrix used for training.
+    - ICM_all: Item-Content matrix (if needed by specific recommenders).
+    - recommenders: Dictionary mapping recommender names to their classes.
+    - GH_PATH: Base path to load best parameter files.
+
+    Returns:
+    - features_recommenders: Dictionary of trained recommenders.
+    """
+    paths_to_best_params = {
+        "RP3beta": "GraphBased",
+        "P3alpha": "GraphBased",
+        "ItemKNNCF": "KNN",
+        "ItemKNNCBF": "KNN",
+        "UserKNNCF": "KNN",
+        "FasterIALS": "MatrixFactorization",
+        "NMF": "MatrixFactorization",
+        "PureSVDItem": "MatrixFactorization/PureSVDRecommender",
+        "ScaledPureSVD": "MatrixFactorization/PureSVDRecommender",
+        "MultVAE": "Neural",
+        "SLIMElasticNet": "SLIM",
+        "SLIM_BPR_Python": "SLIM",
+    }
+    
+    fitted_recommenders = {}
+    
+    for recommender_name, recommender_class in recommenders.items():
+        start_time = time.time()
+        
+        print(f"{recommender_name} Model - TRAINING with its best parameters.")
+        
+        try:
+            # Initialize recommender
+            recommender = recommender_class(URM_train)
+        except Exception as e:
+            recommender = recommender_class(URM_train, ICM_all)
+        
+        # Load best parameters
+        param_file_path = os.path.join(
+            GH_PATH, paths_to_best_params[recommender_name], 
+            f"{recommender_name}Recommender", f"Optimizing{metric}", 
+            f"best_params_{recommender_name}_{metric}.json"
+        )
+        
+        try:
+            with open(param_file_path, 'r') as best_params_json:
+                best_params = json.load(best_params_json)
+        except FileNotFoundError:
+            print(f"Error: Parameter file not found for {recommender_name} at {param_file_path}. Skipping.")
+            continue
+        
+        # Train recommender with best parameters
+        recommender.fit(**best_params)
+        
+        # Save the trained recommender
+        fitted_recommenders[recommender_name] = recommender
+        
+        elapsed_time = time.time() - start_time
+        print(f"Training of {recommender_name} completed in {elapsed_time:.2f} seconds.\n")
+    
+    return fitted_recommenders
+
+
+def create_XGBoost_dataframe(URM, candidate_generator_recommenders, features_recommenders, ICM, reference_URM=None, cutoff=50, categorical=False):
+    """
+    Create a DataFrame for a recommendation system pipeline, including additional feature generation from recommenders.
+
+    Parameters:
+    - URM: Sparse matrix for user-item interactions (training or testing set).
+    - reference_URM: Sparse matrix for user-item interactions (reference set for validation or testing, optional).
+    - candidate_generator_recommenders: Dictionary of recommenders generating candidate recommendations.
+    - features_recommenders: Dictionary of recommenders providing additional features.
+    - ICM: Sparse matrix of item-content features.
+    - cutoff: Number of recommendations to consider from each recommender.
+    - config: Dictionary containing configuration, e.g., for categorical encoding.
+
+    Returns:
+    - interaction_dataframe: DataFrame of features.
+    - groups: Array indicating group sizes by user.
+    """
+    n_users, n_items = URM.shape
+
+    # Initialize DataFrame to store recommendations
+    interaction_dataframe = pd.DataFrame(index=range(0, n_users), columns=["ItemID"])
+    interaction_dataframe.index.name = 'UserID'
+
+    for user_id in tqdm(range(n_users)):
+        recommendations = np.array([])
+
+        # Generate candidate recommendations from candidate generators
+        for candidate_recommender in candidate_generator_recommenders.values():
+            candidate_recommendations = candidate_recommender.recommend(user_id, cutoff=cutoff)
+            recommendations = np.union1d(recommendations, candidate_recommendations)
+
+        interaction_dataframe.loc[user_id, "ItemID"] = recommendations
+
+    # Expand the recommendations into rows
+    interaction_dataframe = interaction_dataframe.explode("ItemID")
+
+    if reference_URM is not None:
+        # Map reference data to user-item pairs for generating labels
+        reference_URM_coo = sps.coo_matrix(reference_URM)
+        correct_recommendations = pd.DataFrame({"UserID": reference_URM_coo.row,
+                                                "ItemID": reference_URM_coo.col})
+
+        # Merge to identify correct recommendations
+        interaction_dataframe = pd.merge(interaction_dataframe, correct_recommendations, on=['UserID', 'ItemID'], how='left', indicator='Exist')
+        interaction_dataframe["Label"] = interaction_dataframe["Exist"] == "both"
+        interaction_dataframe.drop(columns=['Exist'], inplace=True)
+
+        # Set UserID as the index for efficient updates
+        interaction_dataframe = interaction_dataframe.set_index('UserID')
+
+    # Add recommendation scores from feature recommenders
+    for user_id in tqdm(range(n_users)):
+        for rec_label, rec_instance in features_recommenders.items():
+            item_list = interaction_dataframe.loc[user_id, "ItemID"].values.tolist()
+
+            all_item_scores = rec_instance._compute_item_score([user_id], items_to_compute=item_list)
+
+            interaction_dataframe.loc[user_id, rec_label] = all_item_scores[0, item_list]
+
+    # Reset index for further processing
+    interaction_dataframe = interaction_dataframe.reset_index()
+    interaction_dataframe = interaction_dataframe.rename(columns={"index": "UserID"})
+
+    # Add item popularity feature
+    item_popularity = np.ediff1d(sps.csc_matrix(URM).indptr)
+    interaction_dataframe['item_popularity'] = item_popularity[interaction_dataframe["ItemID"].values.astype(int)]
+
+    # Add user profile length feature
+    user_popularity = np.ediff1d(sps.csr_matrix(URM).indptr)
+    interaction_dataframe['user_profile_len'] = user_popularity[interaction_dataframe["UserID"].values.astype(int)]
+
+    # Add content-based features from ICM
+    features_df = pd.DataFrame.sparse.from_spmatrix(ICM)
+    interaction_dataframe = interaction_dataframe.set_index('ItemID').join(features_df, how='inner')
+    interaction_dataframe = interaction_dataframe.reset_index()
+    interaction_dataframe = interaction_dataframe.rename(columns={"index": "ItemID"})
+
+    # Clean and sort data
+    interaction_dataframe = interaction_dataframe.sort_values("UserID").reset_index()
+    interaction_dataframe.drop(columns=['index'], inplace=True)
+
+    # Group size for each user
+    groups = interaction_dataframe.groupby("UserID").size().values
+
+    # Encode categorical features if specified in the config
+    if categorical:
+        interaction_dataframe["UserID"] = interaction_dataframe["UserID"].astype("category")
+        interaction_dataframe["ItemID"] = interaction_dataframe["ItemID"].astype("category")
+
+    return interaction_dataframe, groups
