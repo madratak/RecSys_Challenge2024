f99a1f5f752e0f4cb174d440f0c39bd252e79093
madratak <madratak@gmail.com>
2024-12-28 16:44:32 +0100

    Add best_PureSVDItem_MAP_Train_tuned.zip model file~

--- Utils/xgboost_functions.py
+++ Utils/xgboost_functions.py
@@ -110,18 +110,15 @@ def fit_recommenders(metric, phase, URM_train, ICM_all, recommenders, GH_PATH, t
         zip_file_path = f"/kaggle/working/best_{recommender_name}_{metric}_{phase}_tuned.zip"
 
         # 50MB limitation management for GitHub pushes
-        if (os.path.getsize(zip_file_path) / (1024 * 1024)) < 50:
-            try:
-                upload_file(
-                    zip_file_path,  
-                    saved_model_file_path, 
-                    f"{recommender_name} recommender tuned with best parameters for {phase} (from Kaggle notebook)",
-                    repo
-                )
-            except Exception as e:
-                print(f"Error while uploading {zip_file_path} to GitHub: {e}")
-        else:
-            print(f"\nThe best recommender was not saved on GitHub because its size exceeds 50 MB.")
+        try:
+            upload_file(
+                zip_file_path,  
+                saved_model_file_path, 
+                f"{recommender_name} recommender tuned with best parameters for {phase} (from Kaggle notebook)",
+                repo
+            )
+        except Exception as e:
+            print(f"Error while uploading {zip_file_path} to GitHub: {e}")
 
     return fitted_recommenders
 
