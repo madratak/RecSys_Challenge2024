89e7ad443d1a1983b5e43fa3d452ba453311b13e
madratak <madratak@gmail.com>
2025-01-02 01:06:06 +0100

    DB uploaded

--- Utils/xgboost_functions.py
+++ Utils/xgboost_functions.py
@@ -161,7 +161,7 @@ def fit_recommenders(metric, phase, URM_train, ICM_all, recommenders, GH_PATH, t
         print(f"Training of {recommender_name} completed in {time_value:.2f} {time_unit}.\n")
 
         # Save the trained model locally
-        # recommender.save_model(folder_path='/kaggle/working/', file_name=f"best_{recommender_name}_{metric}_{phase}_tuned")
+        recommender.save_model(folder_path='/kaggle/working/', file_name=f"best_{recommender_name}_{metric}_{phase}_tuned")
 
         # zip_file_path = f"/kaggle/working/best_{recommender_name}_{metric}_{phase}_tuned.zip"
 
